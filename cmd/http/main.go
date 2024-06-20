dd7932a4650f207c6d46b72d4c4ac1d38d70303b
Anderson L. Sergio <asergio@wiley.com>
2024-06-20 20:58:54 -0300

    chore: create initial project directory structure

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -13,6 +13,8 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 
+	// TODO: port should be parsed from env
+	// TODO: call log.Fatal if it's not provided
 	log.Println("Starting server on port 8080")
 	// TODO: listen for syscalls to shutdown server gracefully
 	log.Fatal(http.ListenAndServe(":8080", router))
