bfe9cca0210f9b2a5ea7d1850f357e2ab50aa0cd
Anderson L. Sergio <asergio@wiley.com>
2024-06-23 23:22:09 -0300

    fix: allow HTTP server to gracefully shut down

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
 	httpSwagger "github.com/swaggo/http-swagger"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/hellofreshdevtests/HFtest-platform-anlsergio/api"
 	"github.com/hellofreshdevtests/HFtest-platform-anlsergio/internal/config"
@@ -40,6 +46,38 @@ func main() {
 
 	// start the HTTP server
 	log.Printf("Starting server on port %d", cfg.ServerPort)
-	// TODO: listen for syscalls to shutdown server gracefully
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.ServerPort), r))
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.ServerPort),
+		Handler: r,
+	}
+
+	// Start up the HTTP server in a Go routine
+	// to not block the execution so that the Signal listener can
+	// take it from there.
+	go func() {
+		// When the server exits, make sure the error states that the server
+		// was closed normally, meaning there's no unexpected error.
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+		log.Println("Server is shutting down")
+	}()
+
+	// Listen to OS termination signals to allow for a graceful shutdown
+	// (especially important in Kubernetes runtimes)
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+
+	// define the main context with cancel to release
+	// associated resources upon shutdown.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Call Shutdown for gracefully shut it down.
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown error: %v", err)
+	}
+	log.Println("Server gracefully shutdown complete.")
 }
